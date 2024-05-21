bae2d885354a204f1102104c854469dbb98f7dfa
Bergomezz <bernardogomezz@yahoo.com.br>
2024-05-21 12:15:19 -0300

    feature: mouse usage and the timer count is done

--- main.lua
+++ main.lua
@@ -6,15 +6,43 @@ function love.load()
   target.radius = 50
 
   score = 0
-  timer = 0
+  timer = 10
+
+  gameFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
-  target.x = love.math.random()
-  target.y = love.math.random()
+  if timer > 0 then 
+    timer = timer - dt
+  end
+  
+  if timer < 0 then
+    timer = 0
+  end
 end
 
 function love.draw()
   love.graphics.setColor(1,0,0)
   love.graphics.circle("fill", target.x, target.y, target.radius)
+
+  love.graphics.setColor(1,1,1)
+  love.graphics.setFont(gameFont)
+  love.graphics.print(score, 0,0)
+  love.graphics.print(math.ceil(timer), 300,0)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+  if button == 1 then
+    local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+    if mouseToTarget < target.radius then
+      score = score + 1
+      target.x = love.math.random(target.radius,love.graphics.getWidth() - target.radius)
+      target.y = love.math.random(target.radius,love.graphics.getHeight() - target.radius)
+    end
+  end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+  local d = math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+  return d
 end
