83740d6b0c5e632f9cd5ed6c6ad1fbb66637e0c2
Bergomezz <bernardogomezz@yahoo.com.br>
2024-05-21 12:36:29 -0300

    feature: sprites add and integrated with the game

--- main.lua
+++ main.lua
@@ -9,6 +9,13 @@ function love.load()
   timer = 10
 
   gameFont = love.graphics.newFont(40)
+
+  sprites = {}
+  sprites.sky = love.graphics.newImage("sprites/sky.png")
+  sprites.target = love.graphics.newImage("sprites/target.png")
+  sprites.crosshairs = love.graphics.newImage("sprites/crosshairs.png")
+
+  love.mouse.setVisible(false)
 end
 
 function love.update(dt)
@@ -22,6 +29,7 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.draw(sprites.sky)
   love.graphics.setColor(1,0,0)
   love.graphics.circle("fill", target.x, target.y, target.radius)
 
@@ -29,6 +37,10 @@ function love.draw()
   love.graphics.setFont(gameFont)
   love.graphics.print(score, 0,0)
   love.graphics.print(math.ceil(timer), 300,0)
+
+  love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+  love.graphics.draw(sprites.crosshairs, love.mouse.getX()-20, love.mouse.getY()-20)
+  
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
